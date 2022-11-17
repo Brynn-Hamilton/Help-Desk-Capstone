d3715f66e5440c83709ca353d7056d36285f9de1
Brynn Hamilton <brynnhamilton1@rocketmortgage.com>
2022-11-17 15:00:36 -0500

    Added favorites controller

---
+++ HelpDeskAPI/Controllers/FavoritesController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelpDeskAPI.Controllers
+{
+    [Route("favorites")]
+    [ApiController]
+    public class FavoritesController : ControllerBase
+    {
+        public IEnumerable<Favorites> GetAll()
+        {
+            List<Favorites> Favorites = DAL.GetAllFavorites();
+            return Favorites;
+        }
+
+        [HttpPost]
+        public Favorites Add(Favorites fav)
+        {
+            return DAL.AddFavorite(fav);
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int ticket_id, int user_id)
+        {
+            DAL.DeleteFavorite(ticket_id, user_id);
+        }
+    }
+}
+
